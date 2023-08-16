be3cbf6ce071acc99cd0e00cd925124594e91491
Jim Allanson <jim@amplience.com>
2023-08-16 17:46:47 +0100

    feat: pass an axios-retry client in to DC sdk

--- src/services/dynamic-content-client-factory.ts
+++ src/services/dynamic-content-client-factory.ts
@@ -1,9 +1,14 @@
 /* eslint-disable @typescript-eslint/camelcase */
 import { DynamicContent } from 'dc-management-sdk-js';
 import { ConfigurationParameters } from '../commands/configure';
+import axiosRetry from 'axios-retry';
+import axios from 'axios';
 
-const dynamicContentClientFactory = (config: ConfigurationParameters): DynamicContent =>
-  new DynamicContent(
+const dynamicContentClientFactory = (config: ConfigurationParameters): DynamicContent => {
+  const axiosClient = axios.create();
+  axiosRetry(axiosClient, { retries: 3, retryDelay: axiosRetry.exponentialDelay });
+
+  return new DynamicContent(
     {
       client_id: config.clientId,
       client_secret: config.clientSecret
@@ -11,7 +16,9 @@ const dynamicContentClientFactory = (config: ConfigurationParameters): DynamicCo
     {
       apiUrl: process.env.API_URL,
       authUrl: process.env.AUTH_URL
-    }
+    },
+    axiosClient
   );
+};
 
 export default dynamicContentClientFactory;
